b02e2c50eb6892d6afbe96a74cb8301cd1338a2c
Báder László <bader.laszlo22@gmail.com>
2016-07-13 16:29:53 +0200

    Desktop done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
 $(function(){
+
+	$('.js-agreement').on('click', function  (e) {
+
+		e.preventDefault();
+
+		$('.agreement').fadeOut('fast', function() {
+
+			$('.description').fadeIn('fast');
+			
+		});
+		 
+	});
+
+	$('.js-description').on('click', function  (e) {
+
+		e.preventDefault();
+
+		$('.description').fadeOut('fast', function() {
+
+			$('.game').fadeIn('fast');
+
+			$('header .logo').addClass('game-fix');
+
+			$('header nav').addClass('game-fix');
+
+			$('.overlay').fadeIn('fast');
+
+			$('.overlay').delay(5000).fadeOut('fast', function () {
+				  
+				$('.game').fadeOut('fast');
+
+				$('header .logo').removeClass('game-fix');
+
+				$('header nav').removeClass('game-fix');
+
+				$('.success').fadeIn('fast');
+
+			});
+			
+			
+		});
+		 
+	});
     
 });
